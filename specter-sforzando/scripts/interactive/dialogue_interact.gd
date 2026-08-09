28838990ce5767bdeab95dc835c2d1ad59eb91e7
FoxParadeAFK <s.kittwong004@gmail.com>
2026-08-09 12:51:03 +0100

    Basic project + interaction framework + testing scene

---
+++ specter-sforzando/scripts/interactive/dialogue_interact.gd
@@ -0,0 +1,4 @@
+class_name DialogueInteract extends Interact
+
+func trigger() -> void:
+	pass
---
+++ specter-sforzando/scripts/interactive/interact.gd
@@ -0,0 +1,6 @@
+@abstract
+class_name Interact extends Resource
+
+signal has_interacted
+
+@abstract func trigger() -> void
